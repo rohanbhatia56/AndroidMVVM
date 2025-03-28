200af589ec918bc8b67b0f1b5af92dc0b0040ac0
rohanbhatia56 <bhatiarohan156@gmail.com>
2025-03-28 15:59:59 +0530

    APPV2-16319 replaced links, images, and videos in offline mode

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  rules: {
+    'header-pattern': [2, 'always', /^APPV2-\d{5} .+$/],
+    'header-max-length': [2, 'always', 100],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^APPV2-(\d{5}) (.+)$/,
+      headerCorrespondence: ['ticket', 'subject']
+    }
+  }
+};
